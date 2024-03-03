9edb9572efd27865e0410144e9e061df29f945be
borgonessbo <161991296+borgonessbo@users.noreply.github.com>
2024-03-03 11:06:49 +0800

    first commit base proj
    base web api project, still raw skeleton

---
+++ Dump.Api/Controllers/DumpController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dump.Api.Controllers
+{
+    [ApiController, AllowAnonymous]
+    public class DumpController : Controller
+    {
+        [HttpGet, Route("")]
+       public Boolean Dump()
+       {
+            return false;
+       }
+    }
+}
